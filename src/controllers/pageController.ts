d430057e936fcf71c48b3a76436907f96605a525
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-11-12 19:17:23 -0300

    Ativando Menu

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
 
+import {createMenuObject} from '../helpers/createMenuObject';
+
 
 export const home = (req: Request, res: Response) =>{
     res.render('pages/page', {
+        menu: createMenuObject('all'),
         banner:{
             title: 'Todos os animais',
             background: 'allanimals.jpg'
@@ -12,6 +15,7 @@ export const home = (req: Request, res: Response) =>{
 
 export const dogs = (req: Request, res: Response) =>{
     res.render('pages/page', {
+        menu: createMenuObject('dog'),
         banner:{
             title: 'Cachorros',
             background: 'banner_dog.jpg'
@@ -21,6 +25,7 @@ export const dogs = (req: Request, res: Response) =>{
 
    export const cats = (req: Request, res: Response) =>{
     res.render('pages/page', {
+        menu: createMenuObject('cat'),
         banner:{
             title: 'Gatos',
             background: 'banner_cat.jpg'
@@ -30,6 +35,7 @@ export const dogs = (req: Request, res: Response) =>{
 
    export const fishes = (req: Request, res: Response) =>{
     res.render('pages/page', {
+        menu: createMenuObject('fish'),
         banner:{
             title: 'Peixes',
             background: 'banner_fish.jpg'
