f7f304fed43269ca5f1b78d4bcebb0480ed988b6
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-11-12 15:20:07 -0300

    Separando as views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -2,18 +2,20 @@ import { Request, Response } from 'express';
 
 
 export const home = (req: Request, res: Response) =>{
-    res.send('home no controller!')
- // res.render('pages/pages');
+    res.render('pages/page');
 }
 
 export const dogs = (req: Request, res: Response) =>{
+    res.send('dogs');
     // res.render('pages/pages');
    }
 
    export const cats = (req: Request, res: Response) =>{
+    res.send('cats');
     // res.render('pages/pages');
    }
 
    export const fishes = (req: Request, res: Response) =>{
+    res.send('fishes');
     // res.render('pages/pages');
    }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import{ Router } from 'express';
-import * as PageController from '../controllers/pageControllers';
-import * as SearchController from '../controllers/searchControllers';
+import * as PageController from '../controllers/pageController';
+import * as SearchController from '../controllers/searchController';
 const router = Router();
 
 router.get('/', PageController.home);
