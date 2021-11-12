14b96647addf2d56df469a8ffd7656d7f321f84e
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-11-12 14:54:04 -0300

    base criada

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import{ Router } from 'express';
-
+import * as PageController from '../controllers/pageControllers';
+import * as SearchController from '../controllers/searchControllers';
 const router = Router();
 
-router.get('/', (req, res)=>{
-    res.send('home');
-});
+router.get('/', PageController.home);
+router.get('/dogs', PageController.dogs);
+router.get('/cats', PageController.cats);
+router.get('/fishes', PageController.fishes);
+
+router.get('/search', SearchController.search);
 export default router;
